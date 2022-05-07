6b3ce93b8931efada1f865aab1c0acfa7bbe9496
Anton_Koshelev <antonkoshelev.gli@gmail.com>
2022-05-07 20:22:02 +0200

    STEP-1 create user list component

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useCallback }  from 'react';
 import './rootComponent.scss';
+import UserInfo from './components/userInfo/UserInfo';
+import UsersList from './components/usersList/UsersList';
 
 const RootComponent: React.FC = () => {
+  const [isOpenUserInfo, setIsOpenUserInfo] = useState<boolean>(false);
+  const handleOnItemClick = useCallback(() => {
+      setIsOpenUserInfo(!isOpenUserInfo);
+  }, [])
+
   return (
-    <div className={'wrapper'}>
-    </div>
+      <div className={'wrapper'}>
+        {
+          isOpenUserInfo
+              ? <UserInfo/>
+              : <UsersList handleOnItemClick={handleOnItemClick}/>
+        }
+      </div>
   );
 }
 
