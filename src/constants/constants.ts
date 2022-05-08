23432b314220e36da631250acb8ed0eecb495975
Anton_Koshelev <antonkoshelev.gli@gmail.com>
2022-05-08 08:52:25 +0200

    change token for use application

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const AUTH_TOKEN: string = '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';
+export const AUTH_TOKEN: string = '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';
 export const GIT_HUB_API_URL: string = 'https://api.github.com/';
 export const BACK_ARROW_ICON_URL: string = 'https://cdn.icon-icons.com/icons2/1302/PNG/512/leftarrowsign_85800.png';
